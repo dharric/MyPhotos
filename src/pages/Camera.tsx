000a6ada9f1684765b00e3d104cd75359ded7eb4
David Choi <dharric@live.com>
2020-01-08 22:21:54 -0500

    added storage code

--- src/pages/Camera.tsx
+++ src/pages/Camera.tsx
@@ -6,6 +6,7 @@ import {
   Capacitor
 } from "@capacitor/core";
 import { useCamera, availableFeatures } from "@ionic/react-hooks/camera";
+import { useStorage } from "@ionic/react-hooks/storage";
 import moment from "moment";
 import {
   IonButton,
@@ -25,6 +26,7 @@ import {
 const Camera: React.FC = () => {
   const { Filesystem } = Plugins;
   const { photo, getPhoto } = useCamera();
+  const { get, set } = useStorage();
   const triggerCamera = useCallback(async () => {
     if (availableFeatures.getPhoto) {
       await getPhoto({
@@ -57,10 +59,26 @@ const Camera: React.FC = () => {
             Filesystem.getUri({
               directory: FilesystemDirectory.Data,
               path: fileName
-            }).then(finalPhotoUri => {
+            }).then(async finalPhotoUri => {
               console.log("file uri", finalPhotoUri.uri);
               const photoUrl = Capacitor.convertFileSrc(finalPhotoUri.uri);
               console.log("final photo url", photoUrl);
+
+              const existingImageNames = await get("imageNames");
+              let imageNames = existingImageNames || "";
+              imageNames = imageNames + "," + fileName;
+              await set("imageNames", imageNames);
+              console.log("test image name saved", await get("imageNames"));
+
+              const imagePaths = await get("imagePaths");
+              const imagePathsArray = imagePaths ? JSON.parse(imagePaths) : [];
+              imagePathsArray.push({
+                fileName,
+                photoUrl
+              });
+              const imagePathsArrayString = JSON.stringify(imagePathsArray);
+              await set("imagePaths", imagePathsArrayString);
+              console.log("test image paths saved", await get("imagePaths"));
             });
           });
         })
