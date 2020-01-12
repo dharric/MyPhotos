3f00328a397ad3b30c7320bc93cf326452a27568
David Choi <dharric@live.com>
2020-01-12 18:26:48 -0500

    update format

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
 import {
   IonApp,
   IonIcon,
@@ -8,31 +8,31 @@ import {
   IonTabBar,
   IonTabButton,
   IonTabs
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { photos, camera } from 'ionicons/icons';
-import Tab2 from './pages/Tab2';
-import Details from './pages/Details';
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { photos, camera } from "ionicons/icons";
+import Photos from "./pages/Photos";
+import Details from "./pages/Details";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
-import Camera from './pages/Camera';
+import "./theme/variables.css";
+import Camera from "./pages/Camera";
 
 const App: React.FC = () => (
   <IonApp>
@@ -40,9 +40,13 @@ const App: React.FC = () => (
       <IonTabs>
         <IonRouterOutlet>
           <Route path="/camera" component={Camera} exact={true} />
-          <Route path="/photos" component={Tab2} exact={true} />
+          <Route path="/photos" component={Photos} exact={true} />
           <Route path="/photos/details" component={Details} />
-          <Route path="/" render={() => <Redirect to="/camera" />} exact={true} />
+          <Route
+            path="/"
+            render={() => <Redirect to="/camera" />}
+            exact={true}
+          />
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="camera" href="/camera">
@@ -51,7 +55,7 @@ const App: React.FC = () => (
           </IonTabButton>
           <IonTabButton tab="photos" href="/photos">
             <IonIcon icon={photos} />
-            <IonLabel>Tab Two</IonLabel>
+            <IonLabel>Photos</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
---
+++ src/pages/Photos.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Photos: React.FC = () => {
+  return <div></div>;
+};
+
+export default Photos;
